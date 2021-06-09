448d9676eb8039ccd3cf9dc423d2bb6b39295a2f
Vera-NL <78375434+Vera-NL@users.noreply.github.com>
2021-06-09 13:07:11 +0200

    added dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,24 @@
 import React, {useState} from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary () {
     let [keyword, setKeyword] = useState("");
 
-    function handleKeywordChange (event) {
-        event.preventDefault();
-        setKeyword(event.target.value);
+    function handleResponse (response) {
+        console.log(response.data[0]);
     }
 
     function search (event) {
         event.preventDefault();
-        alert(`Searching for ${keyword} definition`)
+
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_UK/${keyword}`;
+        axios.get(apiUrl).then(handleResponse);
+    }
+
+    function handleKeywordChange (event) {
+        event.preventDefault();
+        setKeyword(event.target.value);
     }
 
     return (
