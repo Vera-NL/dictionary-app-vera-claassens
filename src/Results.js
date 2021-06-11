50d7d03e212e50c6cbb17c8f9f1b693464700418
Vera-NL <78375434+Vera-NL@users.noreply.github.com>
2021-06-11 16:17:40 +0200

    updated the Meaning component

--- src/Results.js
+++ src/Results.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Meaning from "./Meaning"
 
-export default function Results (props) {
+export default function Results(props) {
     if (props.results) {
-        return <div className="Results">
+        return (
+        <div className="Results">
                 <h2>{props.results.word}</h2>
                 {props.results.meanings.map(function (meaning, index) {
                     return (
@@ -12,7 +13,8 @@ export default function Results (props) {
                         </div>
                     );
                 })}
-                </div> 
+            </div> 
+            );
     } else {
         return null; 
     }
